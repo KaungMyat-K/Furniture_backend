a1fe86f39b4bff61a8072b8f48f3d3338c72294c
KaungMyatK <kaungmyat.dev8@gmail.com>
2025-10-08 14:43:51 +0630

    create prj

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express, { urlencoded } from "express";
+import helmet from "helmet";
+import compression from "compression";
+import cors from "cors";
+import morgan from "morgan";
+import { limiter } from "./middlewares/rateLimiter";
+
+export const app = express();
+
+app
+  .use(morgan("dev"))
+  .use(urlencoded({ extended: true }))
+  .use(express.json())
+  .use(cors())
+  .use(helmet())
+  .use(compression())
+  .use(limiter);
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "ready to use" });
+});
